e1986d25a6f2894cc0a19c5cbe3659e92d14698a
soo <aubepluieh3@naver.com>
2022-12-08 01:10:29 +0900

    feat Amount of food

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const nameInput = document.getElementById("name");
 const kgInput = document.getElementById("kg");
-const kcalInput = document.querySelector("#kcal");
-const submit = document.querySelector("#submit");
+const kcalInput = document.getElementById("kcal");
+const submit = document.getElementById("txt");
 
 const DOGNAME_KEY = "dogname";
 
@@ -28,4 +28,12 @@ function day() {
   const kcal = kcalInput.value;
   const g = ageBoxChange() / kcal;
   return g;
+}
+
+//한 번 줄 때 줘야하는
+function food() {
+  let amount = document.querySelector("#amount");
+  let amountValue = count.options[count.selectedIndex].value;
+  const food = day() / amountValue;
+  return food;
 }
