5909a7f573f95c929b98640bdcf6c936060e0541
soo <aubepluieh3@naver.com>
2022-12-07 23:57:19 +0900

    feat DER calc

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-const nameInput = document.querySelector("#name");
-const kgInput = document.querySelector("#kg");
+const nameInput = document.getElementById("name");
+const kgInput = document.getElementById("kg");
+const kcalInput = document.querySelector("#kcal");
 const submit = document.querySelector("#submit");
 
 const DOGNAME_KEY = "dogname";
 
 //기본 에너지 요구량
 function RER() {
-  const kg = kgInput.value();
+  const kg = kgInput.value;
+  console.log(kg);
   const rer = kg * 30 + 70;
   return rer;
+}
+
+//일일 에너지 요구량
+
+function ageBoxChange() {
+  let age = document.querySelector("#age");
+  let ageValue = age.options[age.selectedIndex].value;
+  console.log(ageValue);
+  const der = ageValue * RER();
+  return der;
 }
