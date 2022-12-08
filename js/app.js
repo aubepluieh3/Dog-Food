b62d9b200116e4cb3ce734f7eabbf7d0021a998a
soo <aubepluieh3@naver.com>
2022-12-08 13:30:03 +0900

    feat result

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 const nameInput = document.getElementById("name");
 const kgInput = document.getElementById("kg");
 const kcalInput = document.getElementById("kcal");
-const submit = document.getElementById("txt");
+const mainForm = document.querySelector(".main");
+const submit = document.querySelector("#submit");
+const resultAmountDay = document.getElementById("day");
+const resultAmountTime = document.getElementById("time");
 
 const DOGNAME_KEY = "dogname";
+
+function onresultSubmit(event) {
+  event.preventDefault();
+  const dogname = nameInput.value;
+  const countValue = count.options[count.selectedIndex].value;
+  localStorage.setItem(DOGNAME_KEY, dogname);
+  resultAmountDay.innerText = `${dogname}(이)의 하루 일일 사료급여량은 ${day()}g입니다.`;
+  resultAmountTime.innerText = `하루에 ${countValue}번, ${food()}g씩 주면 됩니다!`;
+}
 
 //기본 에너지 요구량
 function RER() {
@@ -13,7 +25,6 @@ function RER() {
 }
 
 //일일 에너지 요구량
-
 function ageBoxChange() {
   let age = document.querySelector("#age");
   let ageValue = age.options[age.selectedIndex].value;
@@ -26,14 +37,16 @@ function ageBoxChange() {
 //일일 사료급여량
 function day() {
   const kcal = kcalInput.value;
-  const g = ageBoxChange() / kcal;
+  const g = Math.floor(ageBoxChange() / kcal);
   return g;
 }
 
 //한 번 줄 때 줘야하는
 function food() {
-  let amount = document.querySelector("#amount");
-  let amountValue = count.options[count.selectedIndex].value;
-  const food = day() / amountValue;
+  let count = document.querySelector("#count");
+  let countValue = count.options[count.selectedIndex].value;
+  const food = Math.floor(day() / countValue);
   return food;
 }
+
+mainForm.addEventListener("submit", onresultSubmit);
